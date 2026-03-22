342516671991100a3867f1839a17dc51bde07c66
AranaiRa <aranaiseraph@gmail.com>
2026-03-22 06:56:19 -0400

    Attributes loading from data structure, including in JEI!

--- src/main/java/com/aranaira/arcado/item/JewelryItem.java
+++ src/main/java/com/aranaira/arcado/item/JewelryItem.java
@@ -2,23 +2,33 @@ package com.aranaira.arcado.item;
 
 import com.aranaira.arcado.foundation.AttributeData;
 import com.aranaira.arcado.foundation.MetalAlignment;
+import com.google.common.collect.LinkedHashMultimap;
+import com.google.common.collect.Multimap;
 import com.mojang.datafixers.util.Pair;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.jetbrains.annotations.Nullable;
+import top.theillusivec4.curios.api.SlotContext;
+import top.theillusivec4.curios.api.type.capability.ICurioItem;
 
 import java.util.List;
+import java.util.UUID;
 
-public class JewelryItem extends Item {
+import static com.aranaira.arcado.item.JewelItem.JEWEL_DATA;
+
+public class JewelryItem extends Item implements ICurioItem {
     private final int grade;
     private final DyeColor color;
     private final MetalAlignment alignment;
@@ -40,8 +50,20 @@ public class JewelryItem extends Item {
         CompoundTag attributeTag = new CompoundTag();
 
 
-        final Pair<AttributeData, AttributeData> options = JewelItem.JEWEL_DATA.get(color);
+        final Pair<AttributeData, AttributeData> options = JEWEL_DATA.get(color);
         if(alignment == MetalAlignment.ELECTRUM) {
+            for(int i=0; i<2; i++){
+                AttributeData data = i == 0 ? options.getFirst() : options.getSecond();
+                final ResourceLocation key = ForgeRegistries.ATTRIBUTES.getKey(data.getAttribute(grade));
+                if (key != null) {
+                    attributeTag.putString("Slot", "ring");
+                    attributeTag.putString("AttributeName", key.toString());
+                    attributeTag.putString("Name", key.toString());
+                    attributeTag.putDouble("Amount", data.getPure(grade));
+                    attributeTag.putInt("Operation", 0);
+                }
+                attributeList.add(i, attributeTag);
+            }
 
         } else if(alignment != MetalAlignment.NONE) {
             final AttributeData data = alignment == MetalAlignment.GOLD ? options.getFirst() :
@@ -75,10 +97,27 @@ public class JewelryItem extends Item {
     }
 
     @Override
-    public void inventoryTick(ItemStack pStack, Level pLevel, Entity pEntity, int pSlotId, boolean pIsSelected) {
-        super.inventoryTick(pStack, pLevel, pEntity, pSlotId, pIsSelected);
-        if(pLevel.getGameTime() % 10 == 0 && !pStack.getOrCreateTag().contains("CurioAttributeModifiers")) {
-            pStack.getOrCreateTag().put("CurioAttributeModifiers", createAttributeList());
+    public Multimap<Attribute, AttributeModifier> getAttributeModifiers(SlotContext slotContext, UUID uuid, ItemStack stack) {
+        final Multimap<Attribute, AttributeModifier> out = ICurioItem.super.getAttributeModifiers(slotContext, uuid, stack);
+
+        if(stack.getItem() instanceof JewelryItem ji) {
+            final Pair<AttributeData, AttributeData> options = JEWEL_DATA.get(ji.color);
+            if(alignment == MetalAlignment.ELECTRUM) {
+                for(int i=0; i<2; i++) {
+                    final AttributeData data = i == 0 ? options.getFirst() : options.getSecond();
+                    if (data != null) {
+                        out.put(data.getAttribute(grade), new AttributeModifier("arcado", data.getPure(grade), AttributeModifier.Operation.ADDITION));
+                    }
+                }
+            } else if(alignment != MetalAlignment.NONE) {
+                final AttributeData data = alignment == MetalAlignment.GOLD ? options.getFirst() :
+                        (alignment == MetalAlignment.SILVER ? options.getSecond() : null);
+                if (data != null) {
+                    out.put(data.getAttribute(grade), new AttributeModifier("arcado", data.getPure(grade), AttributeModifier.Operation.ADDITION));
+                }
+            }
         }
+
+        return out;
     }
 }
--- src/main/java/com/aranaira/arcado/registry/ItemRegistry.java
+++ src/main/java/com/aranaira/arcado/registry/ItemRegistry.java
@@ -75,6 +75,106 @@ public class ItemRegistry {
             () -> new JewelryItem(new Item.Properties(), 5, DyeColor.RED, MetalAlignment.GOLD)
     );
 
+    public static final RegistryObject<Item> RING_CITRINE_1_GOLD = ITEMS.register("ring_citrine_1_gold",
+            () -> new JewelryItem(new Item.Properties(), 1, DyeColor.YELLOW, MetalAlignment.GOLD)
+    );
+
+    public static final RegistryObject<Item> RING_CITRINE_2_GOLD = ITEMS.register("ring_citrine_2_gold",
+            () -> new JewelryItem(new Item.Properties(), 2, DyeColor.YELLOW, MetalAlignment.GOLD)
+    );
+
+    public static final RegistryObject<Item> RING_CITRINE_3_GOLD = ITEMS.register("ring_citrine_3_gold",
+            () -> new JewelryItem(new Item.Properties(), 3, DyeColor.YELLOW, MetalAlignment.GOLD)
+    );
+
+    public static final RegistryObject<Item> RING_CITRINE_4_GOLD = ITEMS.register("ring_citrine_4_gold",
+            () -> new JewelryItem(new Item.Properties(), 4, DyeColor.YELLOW, MetalAlignment.GOLD)
+    );
+
+    public static final RegistryObject<Item> RING_CITRINE_5_GOLD = ITEMS.register("ring_citrine_5_gold",
+            () -> new JewelryItem(new Item.Properties(), 5, DyeColor.YELLOW, MetalAlignment.GOLD)
+    );
+
+    public static final RegistryObject<Item> RING_PERIDOT_1_GOLD = ITEMS.register("ring_peridot_1_gold",
+            () -> new JewelryItem(new Item.Properties(), 1, DyeColor.LIME, MetalAlignment.GOLD)
+    );
+
+    public static final RegistryObject<Item> RING_PERIDOT_2_GOLD = ITEMS.register("ring_peridot_2_gold",
+            () -> new JewelryItem(new Item.Properties(), 2, DyeColor.LIME, MetalAlignment.GOLD)
+    );
+
+    public static final RegistryObject<Item> RING_PERIDOT_3_GOLD = ITEMS.register("ring_peridot_3_gold",
+            () -> new JewelryItem(new Item.Properties(), 3, DyeColor.LIME, MetalAlignment.GOLD)
+    );
+
+    public static final RegistryObject<Item> RING_PERIDOT_4_GOLD = ITEMS.register("ring_peridot_4_gold",
+            () -> new JewelryItem(new Item.Properties(), 4, DyeColor.LIME, MetalAlignment.GOLD)
+    );
+
+    public static final RegistryObject<Item> RING_PERIDOT_5_GOLD = ITEMS.register("ring_peridot_5_gold",
+            () -> new JewelryItem(new Item.Properties(), 5, DyeColor.LIME, MetalAlignment.GOLD)
+    );
+
+    public static final RegistryObject<Item> RING_TOURMALINE_1_GOLD = ITEMS.register("ring_tourmaline_1_gold",
+            () -> new JewelryItem(new Item.Properties(), 1, DyeColor.BLUE, MetalAlignment.GOLD)
+    );
+
+    public static final RegistryObject<Item> RING_TOURMALINE_2_GOLD = ITEMS.register("ring_tourmaline_2_gold",
+            () -> new JewelryItem(new Item.Properties(), 2, DyeColor.BLUE, MetalAlignment.GOLD)
+    );
+
+    public static final RegistryObject<Item> RING_TOURMALINE_3_GOLD = ITEMS.register("ring_tourmaline_3_gold",
+            () -> new JewelryItem(new Item.Properties(), 3, DyeColor.BLUE, MetalAlignment.GOLD)
+    );
+
+    public static final RegistryObject<Item> RING_TOURMALINE_4_GOLD = ITEMS.register("ring_tourmaline_4_gold",
+            () -> new JewelryItem(new Item.Properties(), 4, DyeColor.BLUE, MetalAlignment.GOLD)
+    );
+
+    public static final RegistryObject<Item> RING_TOURMALINE_5_GOLD = ITEMS.register("ring_tourmaline_5_gold",
+            () -> new JewelryItem(new Item.Properties(), 5, DyeColor.BLUE, MetalAlignment.GOLD)
+    );
+
+    public static final RegistryObject<Item> RING_KUNZITE_1_GOLD = ITEMS.register("ring_kunzite_1_gold",
+            () -> new JewelryItem(new Item.Properties(), 1, DyeColor.PURPLE, MetalAlignment.GOLD)
+    );
+
+    public static final RegistryObject<Item> RING_KUNZITE_2_GOLD = ITEMS.register("ring_kunzite_2_gold",
+            () -> new JewelryItem(new Item.Properties(), 2, DyeColor.PURPLE, MetalAlignment.GOLD)
+    );
+
+    public static final RegistryObject<Item> RING_KUNZITE_3_GOLD = ITEMS.register("ring_kunzite_3_gold",
+            () -> new JewelryItem(new Item.Properties(), 3, DyeColor.PURPLE, MetalAlignment.GOLD)
+    );
+
+    public static final RegistryObject<Item> RING_KUNZITE_4_GOLD = ITEMS.register("ring_kunzite_4_gold",
+            () -> new JewelryItem(new Item.Properties(), 4, DyeColor.PURPLE, MetalAlignment.GOLD)
+    );
+
+    public static final RegistryObject<Item> RING_KUNZITE_5_GOLD = ITEMS.register("ring_kunzite_5_gold",
+            () -> new JewelryItem(new Item.Properties(), 5, DyeColor.PURPLE, MetalAlignment.GOLD)
+    );
+
+    public static final RegistryObject<Item> RING_SPINEL_1_GOLD = ITEMS.register("ring_spinel_1_gold",
+            () -> new JewelryItem(new Item.Properties(), 1, DyeColor.PINK, MetalAlignment.GOLD)
+    );
+
+    public static final RegistryObject<Item> RING_SPINEL_2_GOLD = ITEMS.register("ring_spinel_2_gold",
+            () -> new JewelryItem(new Item.Properties(), 2, DyeColor.PINK, MetalAlignment.GOLD)
+    );
+
+    public static final RegistryObject<Item> RING_SPINEL_3_GOLD = ITEMS.register("ring_spinel_3_gold",
+            () -> new JewelryItem(new Item.Properties(), 3, DyeColor.PINK, MetalAlignment.GOLD)
+    );
+
+    public static final RegistryObject<Item> RING_SPINEL_4_GOLD = ITEMS.register("ring_spinel_4_gold",
+            () -> new JewelryItem(new Item.Properties(), 4, DyeColor.PINK, MetalAlignment.GOLD)
+    );
+
+    public static final RegistryObject<Item> RING_SPINEL_5_GOLD = ITEMS.register("ring_spinel_5_gold",
+            () -> new JewelryItem(new Item.Properties(), 5, DyeColor.PINK, MetalAlignment.GOLD)
+    );
+
     public static final RegistryObject<Item> RING_ONYX_1_GOLD = ITEMS.register("ring_onyx_1_gold",
             () -> new JewelryItem(new Item.Properties(), 1, DyeColor.BLACK, MetalAlignment.GOLD)
     );
@@ -95,6 +195,26 @@ public class ItemRegistry {
             () -> new JewelryItem(new Item.Properties(), 5, DyeColor.BLACK, MetalAlignment.GOLD)
     );
 
+    public static final RegistryObject<Item> RING_ZIRCON_1_GOLD = ITEMS.register("ring_zircon_1_gold",
+            () -> new JewelryItem(new Item.Properties(), 1, DyeColor.WHITE, MetalAlignment.GOLD)
+    );
+
+    public static final RegistryObject<Item> RING_ZIRCON_2_GOLD = ITEMS.register("ring_zircon_2_gold",
+            () -> new JewelryItem(new Item.Properties(), 2, DyeColor.WHITE, MetalAlignment.GOLD)
+    );
+
+    public static final RegistryObject<Item> RING_ZIRCON_3_GOLD = ITEMS.register("ring_zircon_3_gold",
+            () -> new JewelryItem(new Item.Properties(), 3, DyeColor.WHITE, MetalAlignment.GOLD)
+    );
+
+    public static final RegistryObject<Item> RING_ZIRCON_4_GOLD = ITEMS.register("ring_zircon_4_gold",
+            () -> new JewelryItem(new Item.Properties(), 4, DyeColor.WHITE, MetalAlignment.GOLD)
+    );
+
+    public static final RegistryObject<Item> RING_ZIRCON_5_GOLD = ITEMS.register("ring_zircon_5_gold",
+            () -> new JewelryItem(new Item.Properties(), 5, DyeColor.WHITE, MetalAlignment.GOLD)
+    );
+
     public static final RegistryObject<Item> GOLD_PLATED_MUNDANE_BRACELET = ITEMS.register("mundane_bracelet_gold",
             () -> new Item(new Item.Properties())
     );
@@ -105,6 +225,166 @@ public class ItemRegistry {
 
     public static final RegistryObject<Item> SILVER_PLATED_MUNDANE_RING = ITEMS.register("mundane_ring_silver",
             () -> new Item(new Item.Properties())
+    );
+
+    public static final RegistryObject<Item> RING_GARNET_1_SILVER = ITEMS.register("ring_garnet_1_silver",
+            () -> new JewelryItem(new Item.Properties(), 1, DyeColor.RED, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_GARNET_2_SILVER = ITEMS.register("ring_garnet_2_silver",
+            () -> new JewelryItem(new Item.Properties(), 2, DyeColor.RED, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_GARNET_3_SILVER = ITEMS.register("ring_garnet_3_silver",
+            () -> new JewelryItem(new Item.Properties(), 3, DyeColor.RED, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_GARNET_4_SILVER = ITEMS.register("ring_garnet_4_silver",
+            () -> new JewelryItem(new Item.Properties(), 4, DyeColor.RED, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_GARNET_5_SILVER = ITEMS.register("ring_garnet_5_silver",
+            () -> new JewelryItem(new Item.Properties(), 5, DyeColor.RED, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_CITRINE_1_SILVER = ITEMS.register("ring_citrine_1_silver",
+            () -> new JewelryItem(new Item.Properties(), 1, DyeColor.YELLOW, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_CITRINE_2_SILVER = ITEMS.register("ring_citrine_2_silver",
+            () -> new JewelryItem(new Item.Properties(), 2, DyeColor.YELLOW, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_CITRINE_3_SILVER = ITEMS.register("ring_citrine_3_silver",
+            () -> new JewelryItem(new Item.Properties(), 3, DyeColor.YELLOW, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_CITRINE_4_SILVER = ITEMS.register("ring_citrine_4_silver",
+            () -> new JewelryItem(new Item.Properties(), 4, DyeColor.YELLOW, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_CITRINE_5_SILVER = ITEMS.register("ring_citrine_5_silver",
+            () -> new JewelryItem(new Item.Properties(), 5, DyeColor.YELLOW, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_PERIDOT_1_SILVER = ITEMS.register("ring_peridot_1_silver",
+            () -> new JewelryItem(new Item.Properties(), 1, DyeColor.LIME, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_PERIDOT_2_SILVER = ITEMS.register("ring_peridot_2_silver",
+            () -> new JewelryItem(new Item.Properties(), 2, DyeColor.LIME, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_PERIDOT_3_SILVER = ITEMS.register("ring_peridot_3_silver",
+            () -> new JewelryItem(new Item.Properties(), 3, DyeColor.LIME, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_PERIDOT_4_SILVER = ITEMS.register("ring_peridot_4_silver",
+            () -> new JewelryItem(new Item.Properties(), 4, DyeColor.LIME, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_PERIDOT_5_SILVER = ITEMS.register("ring_peridot_5_silver",
+            () -> new JewelryItem(new Item.Properties(), 5, DyeColor.LIME, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_TOURMALINE_1_SILVER = ITEMS.register("ring_tourmaline_1_silver",
+            () -> new JewelryItem(new Item.Properties(), 1, DyeColor.BLUE, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_TOURMALINE_2_SILVER = ITEMS.register("ring_tourmaline_2_silver",
+            () -> new JewelryItem(new Item.Properties(), 2, DyeColor.BLUE, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_TOURMALINE_3_SILVER = ITEMS.register("ring_tourmaline_3_silver",
+            () -> new JewelryItem(new Item.Properties(), 3, DyeColor.BLUE, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_TOURMALINE_4_SILVER = ITEMS.register("ring_tourmaline_4_silver",
+            () -> new JewelryItem(new Item.Properties(), 4, DyeColor.BLUE, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_TOURMALINE_5_SILVER = ITEMS.register("ring_tourmaline_5_silver",
+            () -> new JewelryItem(new Item.Properties(), 5, DyeColor.BLUE, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_KUNZITE_1_SILVER = ITEMS.register("ring_kunzite_1_silver",
+            () -> new JewelryItem(new Item.Properties(), 1, DyeColor.PURPLE, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_KUNZITE_2_SILVER = ITEMS.register("ring_kunzite_2_silver",
+            () -> new JewelryItem(new Item.Properties(), 2, DyeColor.PURPLE, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_KUNZITE_3_SILVER = ITEMS.register("ring_kunzite_3_silver",
+            () -> new JewelryItem(new Item.Properties(), 3, DyeColor.PURPLE, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_KUNZITE_4_SILVER = ITEMS.register("ring_kunzite_4_silver",
+            () -> new JewelryItem(new Item.Properties(), 4, DyeColor.PURPLE, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_KUNZITE_5_SILVER = ITEMS.register("ring_kunzite_5_silver",
+            () -> new JewelryItem(new Item.Properties(), 5, DyeColor.PURPLE, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_SPINEL_1_SILVER = ITEMS.register("ring_spinel_1_silver",
+            () -> new JewelryItem(new Item.Properties(), 1, DyeColor.PINK, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_SPINEL_2_SILVER = ITEMS.register("ring_spinel_2_silver",
+            () -> new JewelryItem(new Item.Properties(), 2, DyeColor.PINK, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_SPINEL_3_SILVER = ITEMS.register("ring_spinel_3_silver",
+            () -> new JewelryItem(new Item.Properties(), 3, DyeColor.PINK, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_SPINEL_4_SILVER = ITEMS.register("ring_spinel_4_silver",
+            () -> new JewelryItem(new Item.Properties(), 4, DyeColor.PINK, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_SPINEL_5_SILVER = ITEMS.register("ring_spinel_5_silver",
+            () -> new JewelryItem(new Item.Properties(), 5, DyeColor.PINK, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_ONYX_1_SILVER = ITEMS.register("ring_onyx_1_silver",
+            () -> new JewelryItem(new Item.Properties(), 1, DyeColor.BLACK, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_ONYX_2_SILVER = ITEMS.register("ring_onyx_2_silver",
+            () -> new JewelryItem(new Item.Properties(), 2, DyeColor.BLACK, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_ONYX_3_SILVER = ITEMS.register("ring_onyx_3_silver",
+            () -> new JewelryItem(new Item.Properties(), 3, DyeColor.BLACK, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_ONYX_4_SILVER = ITEMS.register("ring_onyx_4_silver",
+            () -> new JewelryItem(new Item.Properties(), 4, DyeColor.BLACK, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_ONYX_5_SILVER = ITEMS.register("ring_onyx_5_silver",
+            () -> new JewelryItem(new Item.Properties(), 5, DyeColor.BLACK, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_ZIRCON_1_SILVER = ITEMS.register("ring_zircon_1_silver",
+            () -> new JewelryItem(new Item.Properties(), 1, DyeColor.WHITE, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_ZIRCON_2_SILVER = ITEMS.register("ring_zircon_2_silver",
+            () -> new JewelryItem(new Item.Properties(), 2, DyeColor.WHITE, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_ZIRCON_3_SILVER = ITEMS.register("ring_zircon_3_silver",
+            () -> new JewelryItem(new Item.Properties(), 3, DyeColor.WHITE, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_ZIRCON_4_SILVER = ITEMS.register("ring_zircon_4_silver",
+            () -> new JewelryItem(new Item.Properties(), 4, DyeColor.WHITE, MetalAlignment.SILVER)
+    );
+
+    public static final RegistryObject<Item> RING_ZIRCON_5_SILVER = ITEMS.register("ring_zircon_5_silver",
+            () -> new JewelryItem(new Item.Properties(), 5, DyeColor.WHITE, MetalAlignment.SILVER)
     );
 
     public static final RegistryObject<Item> SILVER_PLATED_MUNDANE_BRACELET = ITEMS.register("mundane_bracelet_silver",
